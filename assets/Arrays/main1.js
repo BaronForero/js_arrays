044baa2bc73f18d3449816ea94d3d2bdae365595
BaronForero <kata.baron.f@gmail.com>
2023-03-14 18:54:34 +0100

    js_arrays

---
+++ assets/Arrays/main1.js
@@ -0,0 +1,125 @@
+
+// uebung_lvl_1_1: Arrays
+// let person = ["loyal", "fun", "jokes"]
+// console.log(person);
+// let friends = ["Horst", "Hans-Peter", "Trudel"]
+// console.log(friends);
+// let favoriteFood = ["Frühschoppen", "Semmelbrösel", "Obazda"]
+// console.log(favoriteFood);
+
+// -------------------------- Übung_lvl_1_2: Arrays Index
+// console.log(person[0]);
+// console.log(person[1]);
+// console.log(person[2]);
+// // 
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+// //
+// console.log(favoriteFood[0]);
+// console.log(favoriteFood[1]);
+// console.log(favoriteFood[2]);
+
+// console.log(person.length);
+// console.log(friends.length);
+// console.log(favoriteFood.length);
+
+// console.log(person);
+// console.log(person.length);
+
+// -------------------------- Codeflow_uebung_lvl_1_4: Arrays push()
+
+// const personTwo = ["fun", "games", "jokes", "humor"];
+// console.log(personTwo.length);
+// personTwo.push("lovesFood", "strongStamina", "dontfuckwithcats", "extremSports");
+
+// let person = ["loyal", "fun", "jokes"]
+// console.log(person.length);
+// person.push("loyal", "alwaysUpForAdventure")
+// console.log(person.length);
+// console.log(person);
+
+// let friends = ["Horst", "Hans-Peter", "Trudel"]
+// console.log(friends);
+// console.log(friends.length);
+// friends.push("Otto", "Wilhelm", "Heinrich")
+// console.log(friends.length);
+
+// let favoriteFood = ["Frühschoppen", "Semmelbrösel", "Obazda"]
+// console.log(favoriteFood);
+// console.log(favoriteFood.length);
+// favoriteFood.push("Münchner Weißwürste", "Bayrischer Leberkäse", "Semmelknödel", "Germknödel")
+// console.log(favoriteFood.length);
+
+
+// -------------------------- uebung_lvl_1_5: Arrays pop()
+
+// let person = ["loyal", "fun", "jokes"]
+// let lastValue = person.pop()
+// console.log(person);
+// console.log(lastValue);
+
+// let friends = ["Horst", "Hans-Peter", "Trudel"]
+// let lastValue1 = friends.pop()
+// console.log(friends);
+// console.log(lastValue1);
+
+// let favoriteFood = ["Frühschoppen", "Semmelbrösel", "Obazda"]
+// let Obazda = favoriteFood.pop()
+// console.log(favoriteFood);
+// console.log(Obazda);
+
+// -------------------------- uebung_lvl_1_6: Arrays shift()
+// let favoriteFood = ["Frühschoppen", "Semmelbrösel", "Obazda"]
+// let Frühschoppen = favoriteFood.shift()
+// console.log(favoriteFood);
+// console.log(Frühschoppen);
+
+// let friends = ["Horst", "Hans-Peter", "Trudel"]
+// let Horst = friends.shift()
+// console.log(friends);
+// console.log(Horst);
+
+// -------------------------- uebung_lvl_1_7: Arrays unshift()
+
+// let friends = ["Horst", "Hans-Peter", "Trudel"]
+// let moreFriends = friends.unshift("Reinhold", "Lisl")
+// console.log(friends);
+//['Reinhold', 'Lisl', 'Horst', 'Hans-Peter', 'Trudel']
+
+// let favoriteFood = ["Frühschoppen", "Semmelbrösel", "Obazda"]
+// let moreFood = favoriteFood.unshift("A hoibe", "Brezn", "Fleischpflanzerl", "Hendl", "A hoxn")
+// console.log(moreFood);
+// console.log(favoriteFood);
+
+// -------------------------- uebung_lvl_1_8: Arrays slice()
+
+// let travelGoals = ["Papua New Guinea", "Vietnam", "Hong Kong", "Bolivia", "Argentina", "Korea", "Guatemala", "YEcuadorPorFavor"]
+// console.log(travelGoals);
+// let twoDestinys = travelGoals.slice(2, 4)
+// console.log(twoDestinys);
+// console.log(travelGoals);
+
+// Original Array wird nicht verändert!!!!!
+
+// -------------------------- uebung_lvl_1_10: Arrays/String split()
+
+// const text = "It's okay, Bender. I like cooking too. Oh, you're a dollar naughtier than most. My fellow Earthicans, as I have explained in my book 'Earth in the Balance', and the much more popular 'Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards."
+
+// const arr1 = text.split(', ')
+// console.log(arr1);
+
+// const arr = text.split(' ')
+// console.log(arr);
+
+// const arr2 = text.split('')
+// console.log(arr2);
+
+
+
+
+
+
+
+
+
